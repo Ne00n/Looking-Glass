aba73dd8d35db1374020730042e1a450d411b940
Neoon <neo@tiefkuehler-madness.me>
2021-10-31 11:51:53 +0100

    always scrap html

--- grabber.py
+++ grabber.py
@@ -22,12 +22,12 @@ def parseUrls(html,type="lg"):
 def get(url):
     try:
         request = requests.get(url,allow_redirects=False,timeout=5)
+        parseUrls(request.text,"scrap")
         if (request.status_code == 200):
             if len(request.text) < 200:
                 print(f"HTML to short {len(request.text)}, dropping {url}")
                 return False
             print(f"Got {request.status_code} keeping {url}")
-            parseUrls(request.text,"scrap")
             return True
         else:
             print(f"Got {request.status_code} dropping {url}")
