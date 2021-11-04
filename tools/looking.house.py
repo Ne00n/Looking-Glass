7a454463a0a64ee6b61b95198e303ff41446ec9c
Neoon <neo@tiefkuehler-madness.me>
2021-11-04 20:44:38 +0100

    update looking

--- tools/looking.house.py
+++ tools/looking.house.py
@@ -12,7 +12,8 @@ for index, row in enumerate(rows):
         link = "looking.house" + list(row.links)[0]
         if not provider in results: results[provider] = {}
         ipv4s = re.findall("([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})",row.text, re.MULTILINE | re.DOTALL)
-        if not link in results[provider]: results[provider][link] = {"ipv4":[ipv4s[0]]}
+        ipv6s = re.findall('([\da-f]{4}:[\da-f]{1,4}:[\d\w:]{1,})',row.text, re.MULTILINE | re.DOTALL)
+        if not link in results[provider]: results[provider][link] = {"ipv4":[ipv4s[0]],"ipv6":ipv6s}
 
 with open(os.getcwd()+'/data/looking.json', 'w') as f:
     json.dump(results, f, indent=4)
