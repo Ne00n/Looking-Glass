ae53b083b19ef1e775b070080eda0d9d3f7f5094
Neoon <neo@tiefkuehler-madness.me>
2021-11-05 02:18:32 +0100

    append on merging

--- grabber.py
+++ grabber.py
@@ -190,8 +190,8 @@ for domain,details in list(data['lg'].items()):
     if not details: del data['lg'][domain]
 
 print("Filter double IP's")
-once = []
 for domain, details in data['lg'].items():
+    once = []
     for url,ips in list(details.items()):
         if ips and ips['ipv4']:
             for ip in list(ips['ipv4']):
@@ -206,7 +206,7 @@ with open(os.getcwd()+'/data/'+default, 'w') as f:
 
 print("Merging files")
 readme = "# Looking-Glass\n"
-list = {}
+list,once = {},{}
 files = os.listdir(os.getcwd()+"/data/")
 for file in files:
     if file.endswith(".json") and not "everything" in file:
@@ -214,9 +214,20 @@ for file in files:
             file = json.loads(handle.read())
         for domain,details in file.items():
             if not domain in list: list[domain] = {}
+            if not domain in once: once[domain] = []
             for url,ips in details.items():
-                if not url in list[domain]: list[domain][url] = {}
-                list[domain][url] = ips
+                if not url in list[domain]: list[domain][url] = {"ipv4":[],"ipv6":[]}
+                if ips:
+                    for ip in ips['ipv4']:
+                        if ip in once[domain]: continue
+                        if not ip in list[domain][url]['ipv4']: list[domain][url]['ipv4'].append(ip)
+                        once[domain].append(ip)
+                    for ip in ips['ipv6']:
+                        if ip in once[domain]: continue
+                        if not ip in list[domain][url]['ipv6']: list[domain][url]['ipv6'].append(ip)
+                        once[domain].append(ip)
+                    if not list[domain][url]['ipv4'] and not list[domain][url]['ipv6']:
+                        del list[domain][url]
 
 print("Updating Readme")
 for element,urls in list.items():
